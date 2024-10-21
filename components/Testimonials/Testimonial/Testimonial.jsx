9fb4b42a40bcf643b1d5a550d96b59b6441d362a
PaulinaCM7 <90063591+PaulinaCM7@users.noreply.github.com>
2024-10-21 15:02:02 -0600

    Fixing route for the images

--- components/Testimonials/Testimonial/Testimonial.jsx
+++ components/Testimonials/Testimonial/Testimonial.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Testimonial({children}){
     return (
         <div className="testimonial">
-            <img className="testimonial--pattern" src={`${process.env.PUBLIC_URL}/images/DotPattern.png`} alt="pattern" />
+            <img className="testimonial--pattern" src="./images/DotPattern.png" alt="pattern" />
             { children }
         </div>
     )
--- components/Testimonials/TestimonialWithImage/TestimonialWithImageSource.jsx
+++ components/Testimonials/TestimonialWithImage/TestimonialWithImageSource.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function TestimonialWithImageSource({ children }){
     return (
         <div className="testimonialwithimage--source">
-            <img className="testimonialwithimage--quoteimg" src="/images/Quote.png" alt="quotes"/>
+            <img className="testimonialwithimage--quoteimg" src="./images/Quote.png" alt="quotes"/>
             { children }
         </div>
     )
